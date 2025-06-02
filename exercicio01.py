95fbde8fff42f170b1e262bb6c11afa5fd7a5fa2
daiara-maia <daiara.maia@escola.pr.gov.br>
2025-06-02 14:11:12 +0000

    terminei

--- exercicio01.py
+++ exercicio01.py
@@ -1,7 +1,13 @@
-# Gabriel está acompanhando o desempenho de dois cursos online que lançou: Python Básico e JavaScript Essencial.
-# Ele quer saber qual curso teve mais avaliações no último mês.
+dia_1 = int(input("dias para atividade 01"))
+dia_2 = int(input("dias para atividade 02"))
+dia_3 = int(input("dia para atividade 03"))
 
-# Crie um programa que receba o número de avaliações de cada curso e exiba qual teve mais.
-# Caso as quantidades sejam iguais, exiba uma mensagem dizendo que houve empate.
+    if dia_01 Ele < or dia_02 < 0 or dia_03 <0:
+      print("erro números negativos")
+    
+    else:
+         soma= dia_01+ dia_02+dia_03
+    print(f"tempo total do projeto:{soma} dias ")ss
+
 
 
